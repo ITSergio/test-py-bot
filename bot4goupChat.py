e2d38fce6e240dd53802ddbb0a9cc9b7b52a5186
ITSergio <33862930+ITSergio@users.noreply.github.com>
2020-04-13 16:12:57 +0300

    Create bot4goupChat.py

---
+++ bot4goupChat.py
@@ -0,0 +1,9 @@
+import telebot
+
+bot=telebot.TeleBot("1121021043:AAGsSjS0IeUFRIoHKXO0pPNAfNgqbj1J1u8")
+
+@bot.message_handler(content_types=["new_chat_members"])
+def handler_new_member(message):
+    user_name = message.new_chat_member.first_name
+    bot.send_message(message.chat.id, "Добро пожаловать, {0}!".format(user_name))
+    
